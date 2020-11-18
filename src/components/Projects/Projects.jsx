8b84f7a513862d8cfd104242802667eb4f29aea9
Øyvind Ege <contact@oyvind-ege.dev>
2020-11-18 13:01:07 +0100

    Fixed responsivity bug in project timeline.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { VerticalTimeline } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import { Container } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
 
@@ -12,25 +11,23 @@ const Projects = () => {
 
   return (
     <section id="projects">
-      <Container>
-        <div className="project-wrapper">
-          <Title title="Projects" />
-          <VerticalTimeline>
-            {projects.map((project, i) => {
-              const timeLineStyle = { background: '#0062cc36', color: '#fff' };
-              const iconButtonStyle = { background: 'rgb(9 33 58)' };
-              return (
-                <Project
-                  key={i}
-                  timeLineStyle={timeLineStyle}
-                  iconButtonStyle={iconButtonStyle}
-                  data={project}
-                />
-              );
-            })}
-          </VerticalTimeline>
-        </div>
-      </Container>
+      <div className="project-wrapper">
+        <Title title="Projects" />
+        <VerticalTimeline>
+          {projects.map((project, i) => {
+            const timeLineStyle = { background: '#0062cc36', color: '#fff' };
+            const iconButtonStyle = { background: 'rgb(9 33 58)' };
+            return (
+              <Project
+                key={i.toString()}
+                timeLineStyle={timeLineStyle}
+                iconButtonStyle={iconButtonStyle}
+                data={project}
+              />
+            );
+          })}
+        </VerticalTimeline>
+      </div>
     </section>
   );
 };
