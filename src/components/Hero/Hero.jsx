1bde52b243cb0ce65580fd167d402ad243a5ac2d
Øyvind Ege <contact@oyvind-ege.dev>
2020-11-20 17:17:24 +0100

    Fixed an issue where the initial Hero text during page load would say 'I am the unknown developer.'

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -25,11 +25,11 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">{name || ''}</span>
             <br />
             {title}
             <br />
-            <span className="text-color-main">{subtitle || "I'm the Unknown Developer."}</span>
+            <span className="text-color-main">{subtitle || ''}</span>
           </h1>
         </Fade>
       </Container>
